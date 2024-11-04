4aba22ec191e49f9298211e7594f8b569ea391ed
Arash <sainiarash555@gmail.com>
2024-11-04 12:26:21 +0530

    myobject

--- project2.js
+++ project2.js
@@ -1,30 +1,30 @@
+const display = document.getElementById("calculator");
 function fun(value) {
-    const display = document.getElementById("calculator");
+    
     display.value += value; 
 }
 
 function cleardisplay() {
-    const display = document.getElementById("calculator");
+    
     display.value = ""; 
 }
 
 function solve() {
-    const display = document.getElementById("calculator");
+    
 
         display.value = eval(display.value); 
 }
 
 function back() {
-    const display = document.getElementById("calculator");
+    
     display.value = display.value.substring(0, display.value.length - 1);
 }
 function fun1(){
-    const display=document.getElementById("calculator");
+
     display.value=Math.sqrt(display.value)
 }
 
 function solve() {
-    const display = document.getElementById("calculator");
     try {
         const a = display.value.replace(/\^/g, '**');
         display.value = eval(a);
@@ -33,7 +33,7 @@ function solve() {
     }
 }
 function power(){
-      const display = document.getElementById("calculator");
+    //   const display = document.getElementById("calculator");
        display.value +="^"
      }
 
